743a146b84b0c56181bc9fdce7f3120896f28694
blackdragonbot <123376007+blackdragonbot@users.noreply.github.com>
2023-01-28 14:01:25 +0530

    Update index.js
    Signed-off-by: blackdragonbot <123376007+blackdragonbot@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 // ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
-//▮ZIM BOT INC 2022 ®️ALL RIGHTS RESERVED
+//▮ZERO TWO 2022 ®️ALL RIGHTS RESERVED
 //▮
 //▮FORK AND DON'T FORGET TO GIVE A STAR
 //▮
 //▮THIS SOFTWARE IS UNDER UZ COPYRIGHT
 //▮
 //▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
-//▮reinhardtuna@mail.uk
-//▮WHATSAPP US : +44 7441 437150
-//▮YOUTUBE CHANNELL: https://youtube.com/c/DRIPSOFC
+//▮kesay15910@breazeim.com
+//▮WHATSAPP US : +94773614270
+//▮YOUTUBE CHANNELL: https://www.youtube.com/@alinesantamd12
 //▮
 //╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 //
@@ -17,8 +17,8 @@
 //┃THIS SOFTWARE INCLUDES 
 //┃SOME ENCRYPTED FILES
 //┃
-//┃THANKS FOR CHOOSING ZIMBOT
-//┃THANKS TO DIKA ARDNT
+//┃THANKS FOR CHOOSING ZERO TWO BOT
+//┃THANKS TO REAL NOTY
 //┗━━━━━━━━━━━━━━━━━━━━━━━━━
 //
 require('./config')
