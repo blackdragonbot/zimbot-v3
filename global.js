c83111cc004b58173a9c516b6bab028b8cdfb5f5
blackdragonbot <123376007+blackdragonbot@users.noreply.github.com>
2023-01-28 13:58:56 +0530

    Update global.js
    Signed-off-by: blackdragonbot <123376007+blackdragonbot@users.noreply.github.com>

--- global.js
+++ global.js
@@ -1,16 +1,16 @@
 //global
 
 // ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
-//▮ZIM BOT INC 2022 ®️ALL RIGHTS RESERVED
+//▮ZERO TWO 2022 ®️ALL RIGHTS RESERVED
 //▮
 //▮FORK AND DON'T FORGET TO GIVE A STAR
 //▮
 //▮THIS SOFTWARE IS UNDER UZ COPYRIGHT
 //▮
 //▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
-//▮reinhardtuna@mail.uk
-//▮WHATSAPP US : +44 7441 437150
-//▮YOUTUBE CHANNELL: https://youtube.com/c/DRIPSOFC
+//▮kesay15910@breazeim.com
+//▮WHATSAPP US : +94773614270
+//▮YOUTUBE CHANNELL: https://www.youtube.com/@alinesantamd12
 //▮
 //╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 //
@@ -18,7 +18,7 @@
 //┃THIS SOFTWARE INCLUDES 
 //┃SOME ENCRYPTED FILES
 //┃
-//┃THANKS FOR CHOOSING ZIMBOT
-//┃THANKS TO DIKA ARDNT
+//┃THANKS FOR CHOOSING ZERO TWO BOT
+//┃THANKS TO REAL NOTY
 //┗━━━━━━━━━━━━━━━━━━━━━━━━━
 //
